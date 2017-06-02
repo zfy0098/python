b921c943078aae8fe6692ba3f83b13ef11a35e11
zfy0098 <zfy0098@163.com>
2017-06-02 17:25:12 +0800

    python

---
+++ com/FibonacciSeries.py
@@ -0,0 +1,10 @@
+
+
+
+a , b = 0 , 1
+
+while b < 100:
+    print(b)
+    tem = a + b
+    a = b
+    b = tem
--- com/loop.py
+++ com/loop.py
@@ -88,6 +88,16 @@ for i,j in enumerate(sequence):
 
 
 
+print("循环语句可以有 else 子句，它在穷尽列表(以for循环)或条件变为 false (以while循环)导致循环终止时被执行,但循环被break终止时不执行。")
+
+for n in range(2,10):
+    for x in range(2,n):
+        if n % x == 0 :
+            print(n, '等于', x, '*', n // x)
+            break
+    else:
+        # 循环中没有找到元素
+        print(n, ' 是质数')
 
 
 
