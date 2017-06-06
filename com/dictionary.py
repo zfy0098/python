cb552ba1af5bff27bb3b859ae37e579ffc4e6046
zfy0098 <zfy0098@163.com>
2017-06-06 21:14:32 +0800

    python字典

--- com/dictionary.py
+++ com/dictionary.py
@@ -3,4 +3,29 @@
 
 dict = {"name" : "zfy" , "age" : 27 }
 
+print("获取字典长度")
+print(len(dict))
+
 print(dict["name"])
+
+dict["name"] = "周芳禹"
+
+print(dict["name"])
+
+
+del dict["age"]
+
+print(dict.keys())
+print(dict.values())
+
+
+print(dict)
+
+
+print("type(dict) : " , type(dict))
+
+
+print("name in dict :" , "name" in dict)
+
+
+print(type("name" in dict))
